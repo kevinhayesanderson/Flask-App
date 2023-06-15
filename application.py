78c998f99211e877e1a78a01e0359abdea9d105f
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-06-15 14:51:01 +0530

    Added default route response

--- application.py
+++ application.py
@@ -5,10 +5,16 @@ This script runs the FlaskTemplate application using a development server.
 from os import environ
 from FlaskTemplate import app
 
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    HOST = environ.get("SERVER_HOST", "localhost")
     try:
-        PORT = int(environ.get('SERVER_PORT', '3000'))
+        PORT = int(environ.get("SERVER_PORT", "3000"))
     except ValueError:
         PORT = 3000
     app.run(HOST, PORT)
